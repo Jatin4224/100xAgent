3ccb7bd1d6ffc811fbf168e350a83f1af21bb328
Jatin4224 <jaikumarsharma94130@gmail.com>
2025-02-01 01:34:29 +0530

    user model

---
+++ backend/models/user.model.js
@@ -0,0 +1,35 @@
+import mongoose from "mongoose";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import "dotenv/config";
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+    lowercase: true,
+    minLength: [6, "EMAIL must be at least 6 letters long"],
+    maxLength: [50, "Email must be no longer more than 50 letters"],
+  },
+  password: {
+    type: String,
+    select: false,
+  },
+});
+
+userSchema.statics.hashpassword = async function (password) {
+  return await bcrypt.hash(password, 10);
+};
+
+userSchema.methods.isValidPassword = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+
+userSchema.methods.generateJwt = function () {
+  return jwt.sign({ email: this.email }, process.env.JWT_SECRET);
+};
+
+const User = mongoose.model("user", userSchema);
+
+export default User;
